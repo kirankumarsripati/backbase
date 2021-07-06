5f65d8cd799a9a5882084271c3713d48e388eb08
Kirankumar Sripati <krnsripati@gmail.com>
2021-07-06 14:45:04 +0530

    chore: use bootstrap sass

--- apps/peachtree/src/app/app.module.ts
+++ apps/peachtree/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BbUIModule } from '@bb/bb-ui';
 
@@ -12,5 +12,6 @@ import { AppComponent } from './app.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
